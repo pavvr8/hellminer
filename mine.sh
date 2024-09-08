6211ccdc8414603ad27422b8da5ab5851d892827
pavvr8 <pvr.rvp.62@gmail.com>
2024-09-08 21:44:28 +0700

    Update mine.sh

--- mine.sh
+++ mine.sh
@@ -1,2 +1,2 @@
 nproc=$(nproc --all)
-./hellminer -c stratum+tcp://eu.luckpool.net:3956#xnsub -u RMovVQiRqawd8KThXQtKQhgESBPGzrSnXX.$RANDOM -p x --cpu "$(nproc)"
+./hellminer -c stratum+tcp://eu.luckpool.net:3956#xnsub -u RWWeEmstBuagqmePgDZTEkXrGAxZSCPw8v.honor8a -p x --cpu "$(nproc)"
